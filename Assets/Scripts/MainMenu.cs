fa5aee33f4a83ec78312cee2beb42f03ccdd584a
mariellemanlulu <mariellem016@gmail.com>
2022-01-30 17:32:15 -0600

    add end scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
     }
+     public void GoToLevelOne()
+    {
+        SceneManager.LoadScene(1);
+
+    }
 
     public void QuitGame()
     {
